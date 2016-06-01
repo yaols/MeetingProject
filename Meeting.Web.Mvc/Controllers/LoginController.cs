a4f9a09df5dca86088ddfec03a6e4a3859f45c37
465212602@qq.com <74xiaobai>
2016-06-01 23:08:38 +0800

    登陆窗体和登陆验证

--- Meeting.Web.Mvc/Controllers/LoginController.cs
+++ Meeting.Web.Mvc/Controllers/LoginController.cs
@@ -1,16 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
 using System.Web.Mvc;
 
 namespace Meeting.Web.Mvc.Controllers
 {
-  
-    public class HomeController : BaseController
+    public class LoginController : BaseController
     {
-
-        public ActionResult LoginVerification() 
+        public ActionResult Login() 
         {
             return View();
         }
