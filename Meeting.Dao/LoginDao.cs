ffdf44cb3f41b07b9eafdd18dad10029954daeeb
465212602@qq.com <74xiaobai>
2016-06-01 23:12:00 +0800

    增加接口类库，登陆操作

--- Meeting.Dao/LoginDao.cs
+++ Meeting.Dao/LoginDao.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Meeting.BLL
+namespace Meeting.Dao
 {
-    public class Class1
+    public class LoginDao
     {
     }
 }
--- Meeting.Interface/LoginInterface.cs
+++ Meeting.Interface/LoginInterface.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Meeting.Dao
+namespace Meeting.Interface
 {
-    public class Class1
+    public interface LoginInterface
     {
+
     }
 }
