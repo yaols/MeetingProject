a4a52575f2604495fbffeb077eb240cfba9177bd
465212602@qq.com <74xiaobai>
2016-06-04 01:37:15 +0800

    会议详�

---
+++ Meeting.Web.Api/Controllers/MeetingInfoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Meeting.Web.Api.Controllers
+{
+    public class MeetingInfoController : Controller
+    {
+        //
+        // GET: /MeetingInfo/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
