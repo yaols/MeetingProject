8f9a88e151584139cd097d2c6b2665e0fedd76fc
465212602@qq.com <74xiaobai>
2016-06-04 10:56:49 +0800

    界面完成

--- Meeting.Web.Api/Controllers/MeetingInfoController.cs
+++ Meeting.Web.Api/Controllers/MeetingInfoController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Meeting.Web.Api.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,8 +14,34 @@ namespace Meeting.Web.Api.Controllers
 
         public ActionResult Index()
         {
-            return View();
+            TitleViewModel model = new TitleViewModel();
+            model.TopTitle = "会议详情";
+            model.Title = "会议详情";
+            return View(model);
         }
+
+        /// <summary>
+        /// 获取材料详情
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult GetMaterial() 
+        {
+            TitleViewModel model = new TitleViewModel();
+            model.Title = "材料详情";
+            model.TopTitle = "材料详情";
+            return View(model);
+        }
+
+
+        public ActionResult GetMaterialImage() 
+        {
+            TitleViewModel model = new TitleViewModel();
+            model.Title = "材料: b20160305b0098";
+            model.TopTitle = "材料: b20160305b0098";
+            model.Type = 1;
+            return View(model);
+        }
+
 
     }
 }
---
+++ Meeting.Web.Api/Models/TitleViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Meeting.Web.Api.Models
+{
+    public class TitleViewModel
+    {
+
+
+        public string Title { get; set; }
+
+        public string TopTitle { get; set; }
+
+        public int Type { get; set; }
+    }
+}
